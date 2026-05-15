0841ee00e0c720db6ccbfe221c5ff02f1c09d1e3
Shivam5566 <141337668+Shivam5566@users.noreply.github.com>
2026-05-15 11:11:23 +0530

    Practical of Selection Sort

---
+++ prl_2.py
@@ -0,0 +1,21 @@
+# Selection Sort with User Input
+
+arr = []
+
+n = int(input("Enter number of elements: "))
+
+for i in range(n):
+    num = int(input("Enter element: "))
+    arr.append(num)
+
+for i in range(n):
+    min_index = i
+
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print("Sorted Array:")
+print(arr)
